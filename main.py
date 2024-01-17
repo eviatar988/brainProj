aba2162cfd39689d89ab021a6dc191671e3472ba
Eviatar Cohen <eviatar988@gmail.com>
2024-01-17 17:58:34 +0200

    גד

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+
 import numpy as np
 import pandas as pd
 import os
@@ -13,20 +14,42 @@ import mne
 from bids_extract import bidsExtract
 
 bids_path = r"C:\Users\eyala\Documents\GitHub\brainProj\ds003688"
-
+dataset = "ds003688"
+subject = "02"
 
 def main():
-    dataset = "ds003688"
-    subject = "02"
+ 
 
     # Download one subject's data from each dataset
     bids_root = op.join(op.dirname(sample.data_path()), dataset)
-    if not op.isdir(bids_root):
-        os.makedirs(bids_root)
+    print(bids_root)
+    test = bidsExtract(bids_root)
+    raw = test.get_raw()
+    #raw.plot() # plot the raw data
+    #print(raw.info)
+    raw_selection = raw["F01", 0:]
+    x1=raw_selection[1]
+    y1=raw_selection[0].T
+    
+    raw_selection = raw["F21", 0:]
+    x2=raw_selection[1]
+    y2=raw_selection[0].T
+    
+    f = y1[0]
+    g = y2[0]
 
-    openneuro.download(dataset=dataset, target_dir=bids_root, include=[f"sub-{subject}"])
-
-    print_dir_tree(bids_root, max_depth=4)
+    
+    #creat cohernce matrix between F01 AND F21
+    f, Cxy = scipy.signal.coherence(f, g, fs=512, nperseg=1024)
+    plt.semilogy(f, Cxy)
+    plt.xlabel('frequency [Hz]')
+    plt.ylabel('Coherence')
+    plt.show()
+    
+    
+    
+    
+    
 if __name__ == '__main__':
     main()
 
