7ff669552904b91240384d765a4e9d36907dd991
eyalarad44 <eyal.arad@live.biu.ac.il>
2024-01-17 20:56:39 +0200

    Create Download_data.py

---
+++ Download_data.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import os
+import os.path as op
+import matplotlib.pyplot as plt
+import openneuro
+from mne_bids import (BIDSPath, read_raw_bids, print_dir_tree, make_report, get_entity_vals)
+import scipy
+from mne.datasets import sample
+import seaborn as sns
+import mne
+from bids_extract import bidsExtract
+
+dataset = "ds003688"
+
+
+# Download one subject's data from each dataset
+bids_root = op.join(op.dirname(sample.data_path()), dataset)
+if not op.isdir(bids_root):
+    os.makedirs(bids_root)
+
+openneuro.download(dataset=dataset, target_dir=bids_root)
