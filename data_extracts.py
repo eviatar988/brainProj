abb3146bddf772870fdcf922c92ba033477a29c8
Eviatar Cohen <eviatar988@gmail.com>
2024-06-16 16:50:01 +0300

    d

--- data_extracts.py
+++ data_extracts.py
@@ -110,7 +110,7 @@ def data_extract(freq_type_rest, freq_type_film, bounds, extract_func):
     film_path = op.join(film_path, patients[first_p])
 
     rest_file = f'{patients[first_p]},task=rest,freq={freq_type_rest}.npz'
-    film_file =  f'{patients[first_p]},task=film,freq={freq_type_rest}.npz'
+    film_file =  f'{patients[first_p]},task=film,freq={freq_type_film}.npz'
     rest_data, film_data = extract_func(rest_path, film_path, rest_file, film_file)
 
     for i in range(first_p+1,last_p+1):
